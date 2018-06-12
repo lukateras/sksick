c3548e29d4be0e31c8a6cf61e41b12ba46c0bcac
Yegor Timoshenko <yegortimoshenko@riseup.net>
2018-06-12 20:03:34 +0000

    Add havoc mode (random bytes), bring back revocation (doesn't work yet)

--- sks-forge-uid/main.go
+++ sks-forge-uid/main.go
@@ -5,14 +5,28 @@ import (
 	"golang.org/x/crypto/openpgp"
 	"golang.org/x/crypto/openpgp/packet"
 	"log"
+	"math/rand"
 	"os"
-)
+) 
+
+var fakeUid = flag.Bool("fakeUid", true, "whether to create a fake UID")
+var uidHavoc = flag.Bool("uidHavoc", false, "use random bytes instead of UID name, comment and email")
+var uidHavocLength = flag.Uint64("uidHavocLength", 2048, "length of each random UID fragment")
+var uidName = flag.String("uidName", "Do not use SKS keyserver sites", "UID name")
+var uidComment = flag.String("uidComment", "no validity checks", "UID comment")
+var uidEmail = flag.String("uidEmail", "@", "UID email")
+
+var fakeRevoke = flag.Bool("fakeRevoke", true, "whether to create a fake revocation certificate")
+var revokeReason = flag.Uint("revokeReason", 2, "see https://tools.ietf.org/html/rfc4880#section-5.2.3.23")
+var revokeReasonText = flag.String("revokeReasonText", "Do not trust this information", "revocation comment")
+
+func randString(length uint64) string {
+	buf := make([]byte, length)
+	rand.Read(buf) // doesn't really fail
+	return string(buf)
+}
 
 func main() {
-	comment := flag.String("comment", "no validity checks", "UID comment")
-	email := flag.String("email", "@", "UID email")
-	name := flag.String("name", "Do not use SKS keyserver sites", "UID name")
-
 	flag.Parse()
 
 	entity, err := openpgp.ReadEntity(packet.NewReader(os.Stdin))
@@ -27,12 +41,38 @@ func main() {
 		sig = v.SelfSignature
 	}
 
-	packet := packet.NewUserId(*name, *comment, *email)
+	if *fakeRevoke {
+		reason := uint8(*revokeReason)
 
-	entity.Identities[packet.Id] = &openpgp.Identity{
-		Name:          packet.Id,
-		UserId:        packet,
-		SelfSignature: sig,
+		revocation := *sig
+		revocation.RevocationReason = &reason
+		revocation.RevocationReasonText = *revokeReasonText
+
+		entity.Revocations = append(entity.Revocations, &revocation)
+	}
+
+	if *fakeUid {
+		var p *packet.UserId
+
+		if *uidHavoc {
+			p = packet.NewUserId(
+				randString(*uidHavocLength),
+				randString(*uidHavocLength),
+				randString(*uidHavocLength))
+		} else {
+			p = packet.NewUserId(*uidName, *uidComment, *uidEmail)
+		}
+
+		entity.Identities[p.Id] = &openpgp.Identity{
+			Name:          p.Id,
+			UserId:        p,
+			SelfSignature: sig,
+		}
+
+		for _, v := range entity.Identities {
+			entity.Identities[p.Id].Signatures =
+				append(entity.Identities[p.Id].Signatures, v.Signatures...)
+		}
 	}
 
 	err = entity.Serialize(os.Stdout)
