ed51eca2e6a4f1d712b11e10f6975d7d590ac8e1
Yegor Timoshenko <yegortimoshenko@riseup.net>
2018-06-13 02:35:09 +0000

    Use URL to Bitbucket issue as default email

--- sks-forge-uid/main.go
+++ sks-forge-uid/main.go
@@ -13,7 +13,7 @@ var randOn = flag.Bool("rand", false, "use random bytes instead of UID name, com
 var randLen = flag.Uint64("randLen", 100000, "length of each random UID fragment")
 var uidName = flag.String("uidName", "Do not use SKS keyserver sites", "UID name")
 var uidComment = flag.String("uidComment", "no validity checks", "UID comment")
-var uidEmail = flag.String("uidEmail", "@", "UID email")
+var uidEmail = flag.String("uidEmail", "https://bitbucket.org/skskeyserver/sks-keyserver/issues/41", "UID email")
 
 func randString(length uint64) string {
 	buf := make([]byte, length)
