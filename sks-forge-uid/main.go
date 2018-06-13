db858d05d173bcd7d8d5b36bab1f2b9670d5c3ee
Yegor Timoshenko <yegortimoshenko@riseup.net>
2018-06-13 02:22:24 +0000

    Remove broken fake revocation code, streamline flag names

--- sks-forge-uid/main.go
+++ sks-forge-uid/main.go
@@ -7,18 +7,13 @@ import (
 	"log"
 	"math/rand"
 	"os"
-) 
+)
 
-var fakeUid = flag.Bool("fakeUid", true, "whether to create a fake UID")
-var uidHavoc = flag.Bool("uidHavoc", false, "use random bytes instead of UID name, comment and email")
-var uidHavocLength = flag.Uint64("uidHavocLength", 2048, "length of each random UID fragment")
+var randOn = flag.Bool("rand", false, "use random bytes instead of UID name, comment and email")
+var randLen = flag.Uint64("randLen", 100000, "length of each random UID fragment")
 var uidName = flag.String("uidName", "Do not use SKS keyserver sites", "UID name")
 var uidComment = flag.String("uidComment", "no validity checks", "UID comment")
 var uidEmail = flag.String("uidEmail", "@", "UID email")
-
-var fakeRevoke = flag.Bool("fakeRevoke", true, "whether to create a fake revocation certificate")
-var revokeReason = flag.Uint("revokeReason", 2, "see https://tools.ietf.org/html/rfc4880#section-5.2.3.23")
-var revokeReasonText = flag.String("revokeReasonText", "Do not trust this information", "revocation comment")
 
 func randString(length uint64) string {
 	buf := make([]byte, length)
@@ -41,38 +36,26 @@ func main() {
 		sig = v.SelfSignature
 	}
 
-	if *fakeRevoke {
-		reason := uint8(*revokeReason)
+	var pak *packet.UserId
 
-		revocation := *sig
-		revocation.RevocationReason = &reason
-		revocation.RevocationReasonText = *revokeReasonText
-
-		entity.Revocations = append(entity.Revocations, &revocation)
+	if *randOn {
+		pak = packet.NewUserId(
+			randString(*randLen),
+			randString(*randLen),
+			randString(*randLen))
+	} else {
+		pak = packet.NewUserId(*uidName, *uidComment, *uidEmail)
 	}
 
-	if *fakeUid {
-		var p *packet.UserId
+	entity.Identities[pak.Id] = &openpgp.Identity{
+		Name:          pak.Id,
+		UserId:        pak,
+		SelfSignature: sig,
+	}
 
-		if *uidHavoc {
-			p = packet.NewUserId(
-				randString(*uidHavocLength),
-				randString(*uidHavocLength),
-				randString(*uidHavocLength))
-		} else {
-			p = packet.NewUserId(*uidName, *uidComment, *uidEmail)
-		}
-
-		entity.Identities[p.Id] = &openpgp.Identity{
-			Name:          p.Id,
-			UserId:        p,
-			SelfSignature: sig,
-		}
-
-		for _, v := range entity.Identities {
-			entity.Identities[p.Id].Signatures =
-				append(entity.Identities[p.Id].Signatures, v.Signatures...)
-		}
+	for _, v := range entity.Identities {
+		entity.Identities[pak.Id].Signatures =
+			append(entity.Identities[pak.Id].Signatures, v.Signatures...)
 	}
 
 	err = entity.Serialize(os.Stdout)
